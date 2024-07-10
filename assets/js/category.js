cbaa8936c65897775919bcaa538c3f58fe4b51ce
bhagyashree <bhagyashreecharolia.gmail.com>
2024-07-10 09:30:02 +0200

    add file and edit file

--- assets/js/category.js
+++ assets/js/category.js
@@ -4,13 +4,16 @@ let categoryToShow = params.get("category");
 document.querySelector("#category").textContent=categoryToShow;
 
 let url = `http://localhost:3000/products?category_like=${categoryToShow}`;
+let urlProduct= `http://localhost:3000/products`;
+let elMenu = document.querySelector("#products");
+// let ulList=document.getElementById("nameList");
 
-function fillFields(menuItem) {
-    document.querySelector("#name").textContent = menuItem.name;
-    document.querySelector("#description").textContent = menuItem.description;
-    document.querySelector("#price").textContent = menuItem.price + " €";
-    document.querySelector("#category").textContent = menuItem.category;
-}
+// function fillFields(menuItem) {
+//     document.querySelector("#name").textContent = menuItem.name;
+//     // document.querySelector("#description").textContent = menuItem.description;
+//     // document.querySelector("#price").textContent = menuItem.price + " €";
+//     // document.querySelector("#category").textContent = menuItem.category;
+// }
 
 fetch(url)
     .then(response => {
@@ -24,7 +27,29 @@ fetch(url)
             }
         }
     })
-    .then(fillFields)
+    .then(productlist)
     .catch(error => {
         alert("The following error occurred: " + error);
     })
+
+    async function fetchProductData() {
+        try {
+            // const response = await fetch(urlProduct);
+            const response = await fetch(url);
+            const data = await response.json();
+            
+            const productList = document.getElementById('nameList');
+            data.forEach(product => {
+                const productElement = document.createElement('div');
+                productElement.innerHTML = `
+                    <h2>${product.modal}</h2>
+                `;
+                productList.appendChild(productElement);
+            });
+        } catch (error) {
+            console.error('Error fetching product data:', error);
+        }
+    }
+
+    // Call the function to fetch and display the data
+    fetchProductData();
